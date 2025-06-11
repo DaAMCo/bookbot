7c58234a0830f1e4f7c3f6872556c2022d22fdb3
DaAMCo <daniel.merma.agronomia@gmail.com>
2025-06-11 08:48:57 -0500

    add word_count

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ def get_book_text(book_path):
 def main():
     book_path = 'books/frankenstein.txt'
     book_text = get_book_text(book_path)
-    print(book_text)
-
+    number_of_words = word_count(book_text)
+    print(f'{number_of_words} words found in the document')
+def word_count(text):
+     # Lee el contenido del archivo 
+    words = text.split() # Devuelve una lista de las palabras del string
+    # El método split() divide el texto en palabras usando espacios como delimitadores
+    # y devuelve una lista de palabras.
+    # Contamos el número de palabras en la lista
+    return len(words) # Devuelve el número de palabras en la lista
 main()
-    
