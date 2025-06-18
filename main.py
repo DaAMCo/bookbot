3a326db82ab1c8acb5696bd6fbf664c869ff67a1
DaAMCo <daniel.merma.agronomia@gmail.com>
2025-06-18 06:13:08 -0500

    first project

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import sys
 # main.py
-from stats import number_of_characters
+from stats import *
 def get_book_text(book_path):
     with open(book_path) as book_file:
         return book_file.read()
 def main():
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
     number_of_character = number_of_characters(book_text)
-    print(number_of_character)
+    total_characters = word_count(book_text)
+    character_orderer = order_dict(number_of_character)
+    # Llama a las funciones de stats.py para obtener el conteo de caracteres
+    print(
+        f"============ BOOKBOT ============\n"
+        f"Analyzing book found at {book_path}\n"
+        f"----------- Word Count ----------\n"
+        f"Found {total_characters} total words\n"
+        f"---------- Character Count -------")
+    for character in character_orderer:
+        for key, value in character.items():
+            print(f"{key}: {value}")
+    print('============= END ===============')
 main()
--- stats.py
+++ stats.py
@@ -7,6 +7,7 @@ def word_count(text):
     # Contamos el número de palabras en la lista
     return len(words) # Devuelve el número de palabras en la lista
 def number_of_characters(text):
+    #valid_letters = set('abcdefghijklmnopqrstuvwxyzæâêëô') # Definimos las letras válidas
     # Cuenta el número de caracteres en el texto
     lowercase_text = text.lower()# Convierte el texto a minúsculas
     # El método lower() convierte todas las letras del texto a minúsculas
@@ -14,9 +15,22 @@ def number_of_characters(text):
     # contar el numero de caracteres por letra
     characters = {} # Creamos un diccionario para almacenar el conteo de caracteres
     for char in lowercase_text:
-        if not char in characters:
-            characters[char] = 1
-        else:
-            characters[char] += 1
-    return print (characters) # Devuelve el diccionario con el conteo de caracteres
+        if char.isalpha(): #verifico si es una letra
+        #if char in valid_letters:
+            if not char in characters:
+                characters[char] = 1
+            else:
+                characters[char] += 1
+    return characters # Devuelve el diccionario con el conteo de caracteres
     # Recorremos el texto y contamos las letras
+def order_dict(characters):
+    dicc_ordered = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
+    list_dict = []  # Creamos una lista para almacenar los diccionarios
+    for key, value in dicc_ordered.items():
+        list_dict.append({key:value})
+    #for i in list_dict:
+     #   for key, value in i.items():
+      #      print(f'{key}: {value}')
+    return list_dict
+    # Ordena el diccionario por el valor de los caracteres
+    
